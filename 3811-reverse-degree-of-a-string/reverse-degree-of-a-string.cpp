a7e3e7f3c85d7a01421c91514455a7f9ec2eacd4
yogeswaralakshmannarra-alt <yogeswaralakshmannarra@gmail.com>
2026-09-21 23:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (78.56%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum=0;
+        for(int i=1;i<=s.size();i++){
+            sum=sum+((26-(s[i-1]-'a'))*i);
+        }
+        return sum;
+    }
+};
